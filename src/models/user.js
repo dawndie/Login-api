b5d61df18401a1b7a434aea6450304cde6ed7df0
dawndie <voluongbang20@gmail.com>
2021-02-02 10:20:01 +0700

    login api finish

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-
-
-const User = mongoose.model('user', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -11,37 +10,66 @@ const User = mongoose.model('user', {
     },
     email: {
         type: String,
+        unique: true,
+        required: true,
         trim: true,
         lowercase: true,
-        required: true,
         validate(value) {
             if (!validator.isEmail(value)) {
-                throw new Error('email is wrong')
+                throw new Error('Email is invalid')
             }
         }
     },
-
     password: {
-        type : String,
+        type: String,
         required: true,
         minlength: 7,
         trim: true,
         validate(value) {
-            if(value.toLowerCase().includes('password')) {
-               throw new Error('password cannot contain password') 
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
-
     age: {
         type: Number,
         default: 0,
         validate(value) {
-            if(value < 0) {
-                throw new Error('age must be a postive number')
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+
+
+const User = mongoose.model('User', userSchema)
 module.exports = User
